258c4e7f7738eb1c18b7357cafda66ebf197e295
Jason Huang <jasson15@gmail.com>
2016-09-28 20:40:43 -0700

    Add Angular App name
    Called ESNApp

---
+++ public/js/controller/directory.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sampleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sampleApp
+ */
+angular.module('ESNApp')
+  .controller('DirectoryCtrl', function ($scope) {
+    $scope.happy = 'fun';
+  });
