0458bf143f9845b32d1d21c956872096fa50853a
SCIN <zhu.scin65536@gmail.com>
2016-10-19 09:53:10 -0700

    delete share status automatically once login

--- public/js/factory/statusService.js
+++ public/js/factory/statusService.js
@@ -9,7 +9,6 @@ app.service('statusService',['$http', 'socketioService',  function ($http,socket
         userName:'User',
         timeStamp:'unknown'
     }
-    if (navigator.geolocation) navigator.geolocation.getCurrentPosition(onPositionUpdate,postStatus,{timeout:1000});
 
     function updateStatus(statusCode) {
         var d = new Date();
