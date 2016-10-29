c9bbbfcdf78565b8767106e9e92687e999a1d49b
Jason Huang <jasson15@gmail.com>
2016-10-28 20:27:56 -0700

    Add jshint and grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,12 +8,20 @@ module.exports = function(grunt) {
     mochacli: {
       all: ['test/*.js'],
     },
+    jshint: {
+      all: ['Gruntfile.js', 'app.js', 'routes/**/*.js', 'lib/**/*.js', 'test/**/*.js'],
+      options: {
+        jshintrc: true,
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-mocha-cli');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
   grunt.registerTask('default', []);
   grunt.registerTask('test', ['mochacli']);
+  grunt.registerTask('lint', ['jshint']);
 
 };
